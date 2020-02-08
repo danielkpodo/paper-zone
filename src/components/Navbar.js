42660482d11c7bee1080d49e3f9280ac82b15ba4
naphtha <naphthanewman@gmail.com>
2020-02-08 22:15:51 +0000

    fix sass issue for project

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navigation">
+      <div className="ui menu">
+        <div className="header item logo">
+          <h1>PaperZone</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
