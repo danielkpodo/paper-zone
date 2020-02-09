3a5b07a093469350ceda0c7a9872a301a280283a
naphtha <naphthanewman@gmail.com>
2020-02-09 10:09:06 +0000

    added color harmony to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -7,7 +7,9 @@ const Logo = () => {
       <Tilt className="Tilt" options={{ max: 55 }}>
         <div className="Tilt-inner">
           <h1>
-            <i className="angle double right icon"></i>PaperZone
+            <i className="angle double right icon"></i>Paper
+            <span style={{ color: "#c6ffdd" }}>&middot;</span>
+            <span style={{ color: " #fbd786" }}>Zone</span>
           </h1>
         </div>
       </Tilt>
