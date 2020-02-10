0ebaf6b4c70fc9f3f1354245f11c1befbe7d3f79
naphtha <naphthanewman@gmail.com>
2020-02-10 11:18:18 +0000

    added media query scss

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,7 +3,7 @@ import dummyImage from "../assets/images/igor-miske-oLhTLD-RBsc-unsplash.jpg";
 
 const Card = () => {
   return (
-    <div className="card">
+    <div className="card hover01">
       <img src={dummyImage} alt="The lord is my shephered" />
     </div>
   );
