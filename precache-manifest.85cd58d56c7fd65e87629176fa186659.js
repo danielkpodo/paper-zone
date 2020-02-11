fdf6cc0cd92d723b90f69566bd9cd9f7535ca34a
naphtha <naphthanewman@gmail.com>
2020-02-11 19:49:52 +0000

    Updates

---
+++ precache-manifest.85cd58d56c7fd65e87629176fa186659.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ff6bd7d1491609b560b96d375a211f",
+    "url": "/paper-zone/index.html"
+  },
+  {
+    "revision": "ca41a5c310b7bbec0dee",
+    "url": "/paper-zone/static/css/2.b4c26766.chunk.css"
+  },
+  {
+    "revision": "ad8f357b5fdbc7bb6804",
+    "url": "/paper-zone/static/css/main.538685a1.chunk.css"
+  },
+  {
+    "revision": "ca41a5c310b7bbec0dee",
+    "url": "/paper-zone/static/js/2.e6c23151.chunk.js"
+  },
+  {
+    "revision": "c6d042cc8c1381d77fdcb5a8ddb704a8",
+    "url": "/paper-zone/static/js/2.e6c23151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8f357b5fdbc7bb6804",
+    "url": "/paper-zone/static/js/main.a297cd87.chunk.js"
+  },
+  {
+    "revision": "2ff4d670f0269730a49c",
+    "url": "/paper-zone/static/js/runtime-main.e9a68f79.js"
+  },
+  {
+    "revision": "3d9dd040665f9445d04534968d97768c",
+    "url": "/paper-zone/static/media/2020 Outline Fortune Kei.3d9dd040.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/paper-zone/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/paper-zone/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/paper-zone/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/paper-zone/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/paper-zone/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/paper-zone/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/paper-zone/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/paper-zone/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/paper-zone/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/paper-zone/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/paper-zone/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/paper-zone/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/paper-zone/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/paper-zone/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/paper-zone/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/paper-zone/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
